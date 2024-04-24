69a755eefa949e45525aea605c87ce5cec57d720
Adriel Leandro Pinzas Morrongiello <adriel.p@geopagos.com>
2024-04-23 20:03:49 -0500

    Initial commit

---
+++ HolaFly-Challenge/App/HolaFly_ChallengeApp.swift
@@ -0,0 +1,16 @@
+//
+//  HolaFly_ChallengeApp.swift
+//  HolaFly-Challenge
+//
+//  Created by Adriel Pinzas on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct HolaFly_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+        }
+    }
+}
